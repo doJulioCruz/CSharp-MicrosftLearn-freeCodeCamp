8e2dd14645b73bacf935fe44ed758e38f537b00e
Julio Cruz <juliocesarlc@outlook.com>
2023-11-24 15:10:32 -0300

    deletando conteudo do repositório

---
+++ Desafio - usando expressões boolianas/Program.cs
@@ -0,0 +1,57 @@
+﻿//minha versão
+
+string permission = "Admin|Manager";
+int level = 100;
+
+if (permission.Contains("Admin") && (level > 55))
+{
+    Console.WriteLine("Welcome, Super Admin user.");
+}
+else if (permission.Contains("Admin") && (level <= 55)) 
+{
+    Console.WriteLine("Welcome, Admin user.");
+}
+else if (permission.Contains("Manager") && (level >= 20))
+{
+    Console.WriteLine("Contact an Admin for access.");
+}
+else if (permission.Contains("Manager") && (level < 20))
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
+else {
+    Console.WriteLine("You do not have permission to be here");
+}
+
+// Outra possivel solução
+/*
+string permission = "Admin|Manager";
+int level = 53;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
+*/
