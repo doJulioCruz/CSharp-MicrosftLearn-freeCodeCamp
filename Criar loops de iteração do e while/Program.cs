418027c6d1536357804729cc02a7bdabf940f4ed
Julio Cruz <juliocesarlc@outlook.com>
2023-11-30 16:18:23 -0300

    Aprendendo sobre instrução DO e WHILE em C#

---
+++ Criar loops de iteração do e while/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+// escrever uma intrução do-while que seja interrompida quando determinado número aleatório for gerado
+
+Random random = new Random();
+int current = 0;
+
+do 
+{
+    current = random.Next(1, 11);
+    Console.WriteLine(current);
+} while (current != 7);
+
+
+
+// Escrever uma instrução while que itere somente enquanto um número aleatório for maior que determinado valor
+
+
+Random random = new Random();
+int current = random.Next(1, 11);
+
+
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = random.Next(1, 11);
+}
+Console.WriteLine($"Last number: {current}");
+
+
+// Usar a instrução continue para passar diretamente para a expressão booliana
+
+Random random = new Random();
+int current = random.Next(1, 11);
+
+do
+{
+    current = random.Next(1, 11);
+    Console.WriteLine("xx");
+    if (current >= 8) continue;
+
+    Console.WriteLine(current);    
+} while (current != 7);
+*/
+
---
+++ Desafio - iterar usando a instrução for/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+// conclusão do desafio
+//====== SOLUÇÃO MICROSOFT ===========
+
+for (int i = 1; i < 101; i++)
+{
+    if ((i % 3 == 0) && (i % 5 == 0))
+        Console.WriteLine($"{i} - FizzBuzz");
+    else if (i % 3 == 0)
+        Console.WriteLine($"{i} - Fizz");
+    else if (i % 5 == 0)
+        Console.WriteLine($"{i} - Buzz");
+    else
+        Console.WriteLine($"{i}");
+}
+
+//======== MINHA SOLUÇÃO ==========
+
+for ( int j = 1; j <= 100; j++)
+{  
+    if ((j % 3 == 0) && (j % 5 == 0))
+    {
+        Console.WriteLine($"{j} - FizzBuzz");
+    }
+    else if (j % 3 == 0)
+    {
+        Console.WriteLine($"{j} - Fizz");
+    }else if (j % 5 == 0)
+    {
+        Console.WriteLine($"{j} - Buzz");
+    }
+    
+     else {
+        Console.WriteLine(j);
+    }
+    
+        
+}
+
+*/
