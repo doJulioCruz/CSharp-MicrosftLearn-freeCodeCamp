831ba2ea95b729c1a131ef96a1fe8d4663ec132c
Julio Cruz <juliocesarlc@outlook.com>
2023-12-26 22:32:26 -0300

    Tipos de dados em C SHARP

---
+++ Escolher o tipo de dados correto em seu código/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+
+//tipo integral com sinal
+
+Console.WriteLine("Signed integral types:");
+
+Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+
+
+//tipo integral sem sinal
+
+Console.WriteLine("");
+Console.WriteLine("Unsigned integral types:");
+
+Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+
+
+//tipos de ponto flutuante
+
+Console.WriteLine("");
+Console.WriteLine("Tipos de ponto flutuante:");
+Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (com ~6-9 digitos de precisão)");
+Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (com 15-17 digitos de precisão)");
+Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (com 28-29 digitos de precisão)");
+
+
+//A palavra-chave new informa ao runtime do .NET que ele deve criar uma instância da matriz int e, em seguida, coordenar-se com o sistema operacional para armazená-la na memória. 
+
+int[] data = new int[3];
+*/
