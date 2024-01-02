4553e71a1360ea53c086c5bda6c002c74badf7ae
Julio Cruz <juliocesarlc@outlook.com>
2024-01-02 18:22:20 -0300

    Conclusão do desafio

--- Desafio - Trabalhar com os dados variáveis em aplicativos de console/Desafio - 1/Program.cs
+++ Desafio - Trabalhar com os dados variáveis em aplicativos de console/Desafio - 1/Program.cs
@@ -1,2 +1,16 @@
-﻿// output - ehT kciuq nworb xof spmuj revo eht yzal god
-//          god yzal eht revo spmuj xof nworb kciuq ehT
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+
+string[] palavras = pangram.Split(' ');
+// Inverte a ordem das palavras
+Array.Reverse(palavras);
+
+// Junta as palavras invertidas de volta em uma string
+string fraseInvertida = String.Join(' ', palavras);
+
+// Converte a string resultante em um array de caracteres
+char[] caracteresInvertidos = fraseInvertida.ToCharArray();
+
+// Inverte a ordem dos caracteres na string final
+Array.Reverse(caracteresInvertidos);
+
+Console.WriteLine(caracteresInvertidos); // Saida - ehT kciuq nworb xof spmuj revo eht yzal god
